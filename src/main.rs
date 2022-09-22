e8fc67b4eb7ee069632348f9fd291f278ab1a7e7
Nils Martel <nilsmartel@yahoo.de>
2022-09-22 16:44:55 +0200

    Test out functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use std::sync::mpsc::channel;
 use std::thread::spawn;
 
@@ -9,6 +10,7 @@ mod db;
 // type InvertedIndex = Map<String, Vec<TableIndex>>;
 
 fn main() {
+    doodlemain();
     let (sender, receiver) = channel();
 
     let mut database = DatabaseCollection::new(db::client(), "gittables_main_tokenized").limit(15);
@@ -25,4 +27,24 @@ fn main() {
     // print_cell_value_overlap_distribution(&inverted_index);
 
     p.join().expect("to join thread");
+}
+
+fn doodlemain() -> ! {
+    let mut client = db::client();
+
+    let table = "gittables_main_tokenized";
+    let rows = client
+        .query(
+            "
+        SELECT * FROM $1 LIMIT 10
+        ",
+            &[&table],
+        )
+        .expect("perform query");
+
+    for row in rows {
+        dbg!(row);
+    }
+
+    std::process::exit(0);
 }
