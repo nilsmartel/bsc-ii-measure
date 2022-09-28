239a681c86eacc0397f851e3db82b13c14bf3925
Nils Martel <nilsmartel@yahoo.de>
2022-09-28 12:59:59 +0200

    Fetch all results at once

--- src/table_lake/database.rs
+++ src/table_lake/database.rs
@@ -28,13 +28,12 @@ impl TableLakeReader for DatabaseCollection {
             self.table, self.limit
         );
 
-        let params: [bool; 0] = [];
-        let mut rows = self
+        let rows = self
             .client
-            .query_raw(&query, params)
+            .query(&query, &[])
             .expect("query database");
 
-        while let Some(row) = rows.next().expect("read next row") {
+        for row in rows {
             // both saved as `integer`
             let table_id: i32 = row.get("tableid");
             let column_id: i32 = row.get("colid");
@@ -49,3 +48,38 @@ impl TableLakeReader for DatabaseCollection {
         }
     }
 }
+
+// slow implementation, that works for big loads.
+// but we have 1/2T RAM. Lets put it to use.
+
+// impl TableLakeReader for DatabaseCollection {
+//     fn read(&mut self, ch: Sender<Entry>) {
+//         let query = format!(
+//             "
+//                 SELECT * FROM {}
+//                 LIMIT {}
+//             ",
+//             self.table, self.limit
+//         );
+
+//         let params: [bool; 0] = [];
+//         let mut rows = self
+//             .client
+//             .query_raw(&query, params)
+//             .expect("query database");
+
+//         while let Some(row) = rows.next().expect("read next row") {
+//             // both saved as `integer`
+//             let table_id: i32 = row.get("tableid");
+//             let column_id: i32 = row.get("colid");
+
+//             // saved as bigint
+//             let row_id: i64 = row.get("rowid");
+
+//             let tokenized = row.get("tokenized");
+//             let index = TableIndex::new(table_id as u32, column_id as u32, row_id as u64);
+
+//             ch.send((tokenized, index)).expect("send index to channel");
+//         }
+//     }
+// }
