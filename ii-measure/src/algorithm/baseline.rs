79cf72f83826c92e068219ad82f8279b00fe1db5
Nils Martel <nilsmartel@yahoo.de>
2022-10-08 15:35:04 +0200

    Attempt to fix anything

--- ii-measure/src/algorithm/baseline.rs
+++ ii-measure/src/algorithm/baseline.rs
@@ -1,24 +1,25 @@
-use std::sync::mpsc::Receiver;
 use crate::table_lake::*;
-use std::time::{ Duration, Instant };
+use std::sync::mpsc::Receiver;
+use std::time::{Duration, Instant};
 
 /// Baseline measure of data, the way it is present in database
 pub fn baseline(receiver: Receiver<(String, TableLocation)>) -> (usize, Duration, Vec<Entry>) {
     let mut ii = Vec::new();
     let mut build_time = Duration::new(0, 0);
+
+    let mut count = 0;
     for data in receiver {
+        if data.0 == "0" {
+            count += 1;
+        }
         let starttime = Instant::now();
         ii.push(data);
 
         build_time += starttime.elapsed();
     }
 
-    // sadly postgres strings are not ordered the  same as rust strings.
-    // we need to order them manually.
-
-    eprintln!("ordering strings now");
-    ii.sort_by_key(|a| a.0.to_string());
-    eprintln!("ordering complete");
+    let percentage = (count as f32 / ii.len() as f32) * 100.0;
+    eprintln!("there were {count} '0' in the set. {percentage}% of the entire set.");
 
     (ii.len(), build_time, ii)
 }
