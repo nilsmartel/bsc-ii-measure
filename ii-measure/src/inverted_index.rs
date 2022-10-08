ef3b06f95908fd66df71862adcd2dfc734f1f459
Nils Martel <nilsmartel@yahoo.de>
2022-10-08 16:24:41 +0200

    Attempt some optimisation

--- ii-measure/src/inverted_index.rs
+++ ii-measure/src/inverted_index.rs
@@ -61,7 +61,9 @@ impl InvertedIndex<Vec<TableLocation>> for Vec<(String, TableLocation)> {
         let endindex =
             binary_search_by_index(self, 0, self.len(), get_end_point, &key).unwrap_or(6);
 
-        self[startindex..endindex].iter().map(|a| a.1).collect()
+        let mut v = Vec::with_capacity(endindex - startindex);
+        v.extend(self[startindex..endindex].iter().map(|a| a.1));
+        v
     }
 }
 
