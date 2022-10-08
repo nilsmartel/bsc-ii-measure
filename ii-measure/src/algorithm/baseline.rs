8cd1b304c06f291f761a56362184905b64ce88f7
Nils Martel <nilsmartel@yahoo.de>
2022-10-08 16:21:50 +0200

    Fix debugging stuff

--- ii-measure/src/algorithm/baseline.rs
+++ ii-measure/src/algorithm/baseline.rs
@@ -7,19 +7,12 @@ pub fn baseline(receiver: Receiver<(String, TableLocation)>) -> (usize, Duration
     let mut ii = Vec::new();
     let mut build_time = Duration::new(0, 0);
 
-    let mut count = 0;
     for data in receiver {
-        if data.0 == "0" {
-            count += 1;
-        }
         let starttime = Instant::now();
         ii.push(data);
 
         build_time += starttime.elapsed();
     }
-
-    let percentage = (count as f32 / ii.len() as f32) * 100.0;
-    eprintln!("there were {count} '0' in the set. {percentage}% of the entire set.");
 
     (ii.len(), build_time, ii)
 }
