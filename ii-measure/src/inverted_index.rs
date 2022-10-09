4582ebfdd837c4c1a12c9a7f8ac658f2c062f7b5
Nils Martel <nilsmartel@yahoo.de>
2022-10-09 13:57:28 +0200

    More logging

--- ii-measure/src/inverted_index.rs
+++ ii-measure/src/inverted_index.rs
@@ -61,6 +61,13 @@ impl InvertedIndex<Vec<TableLocation>> for Vec<(String, TableLocation)> {
         let endindex =
             binary_search_by_index(self, 0, self.len(), get_end_point, &key).unwrap_or(6);
 
+        eprintln!(
+            "=> '{key}'\n[{}]: {}..{}",
+            endindex - startindex,
+            startindex,
+            endindex
+        );
+
         let mut v = Vec::with_capacity(endindex - startindex);
         v.extend(self[startindex..endindex].iter().map(|a| a.1));
         v
