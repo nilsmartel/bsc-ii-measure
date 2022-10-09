03eaea29ec79784f1bd783940875df9378f9400e
Nils Martel <nilsmartel@yahoo.de>
2022-10-09 17:15:26 +0200

    Include ability to render histograms from bintables

--- bintable-info/src/main.rs
+++ bintable-info/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 use bintable::*;
 use structopt::StructOpt;
 
@@ -9,10 +11,17 @@ struct Config {
 
     #[structopt(short, long)]
     header: bool,
+
+    #[structopt(long)]
+    histogram: Option<String>,
 }
 
 fn main() {
-    let Config { table, header } = Config::from_args();
+    let Config {
+        table,
+        header,
+        histogram,
+    } = Config::from_args();
 
     if header {
         println!("table;values;distinct_values;mean_cardinality;avg_cell_len;avg_tableid;avg_colid;avg_rowid");
@@ -28,6 +37,8 @@ fn main() {
     let mut total_length_colid: u64 = 0;
     let mut total_length_rowid: u64 = 0;
 
+    let mut hist = Stats::new();
+
     {
         let mut last_value = String::new();
         for row in bintable {
@@ -35,6 +46,10 @@ fn main() {
             total_length_tableid += row.tableid as u64;
             total_length_colid += row.colid as u64;
             total_length_rowid += row.rowid as u64;
+
+            hist.table(row.tableid as u64);
+            hist.col(row.colid as u64);
+            hist.row(row.rowid as u64);
 
             if row.tokenized != last_value {
                 last_value = row.tokenized;
@@ -54,4 +69,66 @@ fn main() {
     let rowid = total_length_rowid as f64 / values;
 
     println!("{table};{values};{distinct_values};{mean_cardinality};{cell_len};{tableid};{colid};{rowid}");
+
+    if histogram.is_none() {
+        return;
+    }
+
+    let histogram = histogram.unwrap();
+
+    let mut f = File::create(histogram).expect("create file for histogram");
+    use std::io::Write;
+
+    writeln!(&mut f, "rows;cols;tableids").expect("write header of histogram");
+
+    for i in 0..BINS {
+        writeln!(
+            &mut f,
+            "{};{};{}",
+            hist.row_bins[i], hist.row_bins[i], hist.row_bins[i],
+        )
+        .expect("to write row of histogram");
+    }
+}
+
+const BINS: usize = 256;
+const BIN_SPAN: u64 = 16;
+
+#[derive(Debug)]
+struct Stats {
+    row_bins: [u64; BINS],
+    col_bins: [u64; BINS],
+    tableid_bins: [u64; BINS],
+}
+
+impl Stats {
+    pub fn new() -> Self {
+        Stats {
+            row_bins: [0; BINS],
+            col_bins: [0; BINS],
+            tableid_bins: [0; BINS],
+        }
+    }
+
+    pub fn row(&mut self, id: u64) {
+        Stats::log_stat(id, &mut self.row_bins);
+    }
+
+    pub fn col(&mut self, id: u64) {
+        Stats::log_stat(id, &mut self.col_bins);
+    }
+
+    pub fn table(&mut self, id: u64) {
+        Stats::log_stat(id, &mut self.tableid_bins);
+    }
+
+    fn log_stat(value: u64, bins: &mut [u64]) {
+        let mut value = value / BIN_SPAN;
+
+        if value >= bins.len() as u64 {
+            value = bins.len() as u64 - 1;
+        }
+
+        bins[value as usize] += 1;
+    }
 }
