d992c9866ec234ee285c05c995d18e87d9e68e01
Nils Martel <nilsmartel@yahoo.de>
2022-10-11 16:04:54 +0200

    Implement new streaming format

--- bintable2/src/lib.rs
+++ bintable2/src/lib.rs
@@ -3,3 +3,4 @@ mod tablerow;
 
 pub use bintable::BinTable;
 pub use tablerow::TableRow;
+pub use tablerow::ParseAcc;
--- table-to-file-2/src/main.rs
+++ table-to-file-2/src/main.rs
@@ -1,6 +1,8 @@
 mod db;
 
-use bintable::TableRow;
+use std::io::Write;
+
+use bintable2::TableRow;
 use sqlx::{postgres::PgPoolOptions, FromRow, Postgres};
 use sqlx::{postgres::PgRow, Row};
 use tokio_stream::StreamExt;
@@ -30,61 +32,32 @@ async fn main() -> Result<(), sqlx::Error> {
         .connect(&db::client_str())
         .await?;
 
-    let mut output = std::fs::File::create(&config.outfile).expect("to create outfile");
+    let output = std::fs::File::create(&config.outfile).expect("to create outfile");
+    let mut output = std::io::BufWriter::new(output);
 
     let query = format!(
         "SELECT tokenized, tableid, colid, rowid
             FROM {corpus}"
     );
-    eprintln!("{query}");
+    println!("{query}");
 
-    let query = sqlx::query_as::<_, RowWrap>(&query);
+    let query = sqlx::query_as::<_, TableRow>(&query);
 
     let mut stream = query.fetch(&pool);
 
     let mut count = 0;
-    while let Some(RowWrap(row)) = stream.try_next().await? {
-        row.write_bin(&mut output).expect("write to outfile");
+    let mut acc = bintable2::ParseAcc::default();
+    while let Some(row) = stream.try_next().await? {
+        row.write_bin(&mut output, &mut acc)
+            .expect("write to outfile");
 
         count += 1;
         if count & 0xfff == 0 {
-            eprintln!("{count} rows");
+            println!("{count} rows");
         }
     }
 
+    output.flush().expect("flush file");
+
     Ok(())
-}
-
-struct RowWrap(TableRow);
-
-impl<'r> FromRow<'r, PgRow> for RowWrap {
-    fn from_row(row: &'r PgRow) -> Result<Self, sqlx::Error> {
-        let tokenized: Option<String> = row.try_get(0)?;
-        let tokenized = tokenized.unwrap_or_default();
-
-        let tableid = get_number(row, 1) as u32;
-        let colid = get_number(row, 2) as u32;
-        let rowid = get_number(row, 3) as u64;
-
-        Ok(Self(TableRow {
-            tokenized,
-            tableid,
-            colid,
-            rowid,
-        }))
-    }
-}
-
-fn get_number(row: &PgRow, index: usize) -> i64 {
-    if let Ok(v) = row.try_get::<i64, usize>(index) {
-        return v;
-    }
-    if let Ok(v) = row.try_get::<i32, usize>(index) {
-        return v as i64;
-    }
-    if let Ok(v) = row.try_get::<i8, usize>(index) {
-        return v as i64;
-    }
-
-    row.try_get::<i16, usize>(index).unwrap() as i64
 }
