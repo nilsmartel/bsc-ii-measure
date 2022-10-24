8df0bbec288fa9714c97be52e51b263daf797f82
Nils Martel <nilsmartel@yahoo.de>
2022-10-24 14:57:02 +0200

    Include ci scripts

--- ci/build.sh
+++ ci/build.sh
@@ -5,3 +5,9 @@ cargo install --path . --force
 cd ../table-to-file-2
 cargo build
 cargo install --path . --force
+
+cd ../subportion-bintable
+cargo install --path . --force
+
+cd ../sort-bintable
+cargo install --path . --force
