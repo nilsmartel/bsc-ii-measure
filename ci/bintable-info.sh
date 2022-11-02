e922ead795104fb5ab84815a0e3207d3e0f4155d
Nils Martel <nilsmartel@yahoo.de>
2022-11-02 16:55:06 +0100

    Simplify buildscript

--- ci/bintable-info.sh
+++
@@ -1,3 +0,0 @@
-
-cd ./bintable-info
-cargo install --path . --force
--- ci/build.sh
+++ ci/build.sh
@@ -1,15 +1,6 @@
+bash -c '
+cd fastpfor
+bash ./ci/**'
 
-# cd table-to-file-2
-# cargo install --path . --force &
-
-cd ./subportion-bintable
-cargo install --path . --force &
-
-cd ../sort-bintable
-cargo install --path . --force &
-
-cd ../fastpfor
-bash ./ci/**
-
-cd ../ii-measure
-cargo install --path . --force &
+for f in *
+    bash -c "cd $f; cargo install --path . --force" &
